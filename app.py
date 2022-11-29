685ddd5ac48adb48d3ade6311dd5789da9ee36b8
Manomay <manomayjamble15@gmail.com>
2022-11-29 19:05:34 +0530

    Less accurate model

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import re
 from tensorflow.keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 import pickle as pkl
+import os
+import tensorflow as tf
+import subprocess
+import urllib.request
 
 st.write("Music mood classifier")
 message_text = st.text_input("Enter the lyrics of your song: ")
@@ -19,20 +23,21 @@ def preprocess_text(sen):
 
     return sentence
 
-model = load_model('new_model.h5')
+model = pkl.load(open("model.pkl", "rb"))
+
 tokenizer = pkl.load(open('tokenizer.pkl', 'rb'))
 
 def tokenize(text):
-   tokens = tokenizer.text_to_sequence(text)
+   tokens = tokenizer.texts_to_sequences(text)
    arr = pad_sequences(tokens, padding='post', maxlen=200)
    return arr
 
 def classify_message(model, message):
   labels = model.predict(tokenize(message))
-  value = labels.index(max(labels))
+  # value = labels.index(max(labels))
 #   spam_prob = model.predict_proba([message])
   preds = ['Depressed', 'Sad', 'Happy', 'Cheerful']
-  return preds[value]
+  return preds[int(labels[0])]
 
 if message_text != '':
   result = classify_message(model, message_text)
