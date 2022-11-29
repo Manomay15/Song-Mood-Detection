8f81fd27db149b4cff7e0b49a5b37183774d7218
Manomay <manomayjamble15@gmail.com>
2022-11-29 22:29:45 +0530

    Regression added

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def preprocess_text(sen):
     return sentence
 
 model = pkl.load(open("xgboost.pkl", "rb"))
-
+valence_model = pkl.load(open("dvalence.pkl", "rb"))
 tokenizer = pkl.load(open('tokenizer.pkl', 'rb'))
 
 x = []
@@ -31,7 +31,19 @@ for sen in sentences:
 x = tokenizer.texts_to_sequences(x)
 x = pad_sequences(x, padding='post', maxlen=200)
 
-labels = ['Depressing', 'Sad', 'Happy', 'Cheerful']
+labels = ['depressing', 'sad', 'happy', 'cheerful']
+
+valence = valence_model.predict(x)[0]
+
+if valence <= 0.25:
+    ind = 0
+if valence > 0.25 and valence <= 0.5:
+    ind = 1
+if valence > 0.5 and valence <= 0.75:
+    ind = 2
+if valence > 0.75:
+    ind = 3
 
 if message_text != '':
-  st.write(labels[model.predict(x)[0]])
+  # st.write("The Song Appears To Be Pretty ", labels[model.predict(x)[0]])
+  st.write("The spotify valence score is: ", valence_model.predict(x)[0], " which means the song is pretty ", labels[ind], "!")
